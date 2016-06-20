5a52bf4271ac10388c47799a39d0b08ffa0f6f54
김양일 <@220.95.102.49>
2016-06-20 11:22:06 +0900

    completion

--- Java_TP/src/Calculator.java
+++ Java_TP/src/Calculator.java
@@ -1,85 +1,138 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.awt.*;
 
-public class Calculator extends JFrame implements ActionListener {
-	private JPanel panel;
-	private JPanel calpanel;
-	private JPanel valuepanel;
-	private JPanel btnpanel;
-
-	private JTextField calfield;
-	private JTextField valuefield;
-
-	private JTextField temp;
-	private JTextField operator;
-	private JTextField back;
-
-	private JButton jbutton[];
-
-	String[] button = { "7", "8", "9", "" , "C" , "4", "5", "6", "/",  "*", "1", "2", "3", "-", "+", "" , "0", "" , ".", "="};
-
-	private double answer = 0;
-	private double k;
+class Calculator extends JFrame implements ActionListener {
+	JFrame frame;
+	private String first = "";
+	private String result = "";
+	JTextField calfield;
+	JTextField valuefield;
+	GridLayout grid;
+	JButton But;
+	JPanel btnpanel;
+	JPanel textpanel;
+	ArrayList<Integer> ee = new ArrayList<Integer>();
+	ArrayList<String> store = new ArrayList<String>();
+	String[] button = { "7", "8", "9", "", "C", "4", "5", "6", "/", "*", "1", "2", "3", "-", "+", "", "0", "", ".",
+			"=" };
 
 	public Calculator() {
-		super("Calculator");
-		setLayout(new BorderLayout());
-
-		panel = new JPanel();
-		panel.setLayout(new GridLayout(2, 1));
-
-		calpanel = new JPanel();
-		valuepanel = new JPanel();
-
-		calfield = new JTextField(20);
-		valuefield = new JTextField(20);
-
-		valuefield.setHorizontalAlignment(JTextField.RIGHT);
-
-		temp = new JTextField();
-		back = new JTextField();
-
+		frame = new JFrame("Calculator");
+		calfield = new JTextField("");
+		valuefield = new JTextField("0");
+		textpanel = new JPanel();
 		btnpanel = new JPanel();
-		btnpanel.setLayout(new GridLayout(4, 5));
+		valuefield = new JTextField("0");
+		calfield = new JTextField("");
+	}
 
-		jbutton = new JButton[21];
+	// ==========================GUI======================================================================//
+	public void gui() {
+		valuefield.setHorizontalAlignment(JTextField.RIGHT);
+		valuefield.setEditable(false);
+		calfield.setHorizontalAlignment(JTextField.RIGHT);
+		calfield.setEditable(false);
 
-		panel.add(calpanel);
-		panel.add(valuepanel);
+		textpanel.setLayout(new BorderLayout());
+		textpanel.add(BorderLayout.NORTH, calfield);
+		textpanel.add(BorderLayout.CENTER, valuefield);
 
-		operator = new JTextField();
-
-		operator.setText("");
+		btnpanel.setLayout(new GridLayout(4, 3, 6, 6));
 
 		for (int i = 0; i < button.length; i++) {
-			jbutton[i] = new JButton(button[i]);
-			jbutton[i].addActionListener(this);
-			btnpanel.add(jbutton[i]);
+			But = new JButton(button[i]);
+			But.addActionListener(this);
+			btnpanel.add(But);
 		}
-		
-		calpanel.add(calfield);
-		valuepanel.add(valuefield);
 
-		add(panel, BorderLayout.NORTH);
-		add(btnpanel, BorderLayout.CENTER);
+		frame.add(textpanel, BorderLayout.NORTH);
+		frame.add(btnpanel, BorderLayout.CENTER);
+		frame.setSize(300, 200);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
+	// ===========================================================연산자,숫자
+	// listener===========================================
 	public void actionPerformed(ActionEvent e) {
 		String s = e.getActionCommand();
-		//수 입력
-		if (s == "1" || s == "2" || s == "3" || s == "4" || s == "5" || s == "6" || s == "7" || s == "8" || s == "9"
-				|| s == "0" || s == ".") {
-			calfield.setText(calfield.getText() + s);
-			temp.setText(temp.getText() + s);
+		String read;
+
+		try {
+
+			if (s == "1" || s == "2" || s == "3" || s == "4" || s == "5" || s == "6" || s == "7" || s == "8" || s == "9"
+					|| s == "0" || s == ".") {
+				calfield.setText(first);
+				calfield.getText();
+				valuefield.setText("");
+				valuefield.getText();
+
+				read = calfield.getText();
+				first = read + s;
+				calfield.setText(first);
+				calfield.getText();
+			}
+
+			if (s == "/" || s == "*" || s == "-" || s == "+" || s == "=") {
+				valuefield.setText(s);
+				valuefield.getText();
+
+				ee.add(Integer.parseInt(first));
+				first = "";
+
+				store.add(s);
+			}
+
+			if (s == "=") {
+				int sum = 0;
+				sum = ee.get(0);
+
+				for (int a = 0, h = 1; a < ee.size(); a++, h++) {
+					int ve = store.size();
+					if (ve > 0) {
+						ve--;
+
+						if (store.get(a) == "+") {
+							sum = sum + ee.get(h);
+						} else if (store.get(a) == "-") {
+							sum = sum - ee.get(h);
+						} else if (store.get(a) == "*") {
+							sum = sum * ee.get(h);
+						} else if (store.get(a) == "/") {
+							try {
+								sum = sum / ee.get(h);
+							} catch (Exception exc) {
+								sum = 0;
+							}
+						}
+					}
+				}
+				result = sum + "";
+				calfield.setText(result);
+				calfield.getText();
+			}
+
+			if (s == "C") {
+				first = "";
+				calfield.setText("0");
+				calfield.getText();
+				valuefield.setText("");
+				valuefield.getText();
+				ee.clear();
+				store.clear();
+			}
+
+		} catch (Exception ex) {
+			calfield.setText("Error");
+			calfield.getText();
 		}
 
 	}
-	
+
 	public static void main(String[] args) {
 		Calculator c = new Calculator();
-		c.setSize(250, 300);
-		c.setVisible(true);
+		c.gui();
 	}
-
 }
